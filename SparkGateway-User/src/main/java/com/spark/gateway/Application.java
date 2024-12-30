da04dd22c71cfdd1cd628d57b00e82a1ec50447e
swapend <1297598740@qq.com>
2024-12-30 12:43:22 +0800

    feat: 添加用户微服务应用主类并配置应用属性
    - 新增 Application.java 文件，定义用户微服务的启动类
    - 添加应用属性配置，包括服务器端口和服务名称
    - 集成 Nacos 服务发现，配置 Nacos 服务器地址

---
+++ SparkGateway-User/src/main/java/com/spark/gateway/Application.java
@@ -0,0 +1,18 @@
+package com.spark.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: spark
+ * @date: 2024/12/30 11:28
+ * @description:
+ **/
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
